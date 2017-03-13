c292ba579af620f68168a82ec251fada672f67e6
Tõnis Tobre <tobre@bitweb.ee>
2017-03-13 23:31:02 +0200

    Removed unused import

--- src/main/java/io/pdfapi/sdk/PdfApiClient.java
+++ src/main/java/io/pdfapi/sdk/PdfApiClient.java
@@ -10,7 +10,6 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 /**
